59c799b6390f6f31a31cc723fb24c6e4e37e0d20
Avinash <avi.g751@gmail.com>
2025-02-14 17:09:14 +0400

    movment is fucntionally done

--- Assets/Project/Scripts/Player Scripts/AirMovement.cs
+++ Assets/Project/Scripts/Player Scripts/AirMovement.cs
@@ -1,23 +1,44 @@
 using CustomInspector;
+using EditorAttributes;
 using UnityEngine;
+using ReadOnly = CustomInspector.ReadOnlyAttribute;
 
 [RequireComponent(typeof(Rigidbody))]
 public class AirMovement : MonoBehaviour
 {
     [SelfFill][SerializeField] Rigidbody rb;
 
-    [HorizontalLine("Movement Stats", 3, FixedColor.CherryRed)]
+    [HorizontalLine("Steering Stats", 3, FixedColor.DustyBlue)]
     [SerializeField] float forwardSpeed = 10f;
     [SerializeField] float rollSteeringSpeed = 5f;
     [SerializeField] float yawSteeringSpeed = 2f;
     [SerializeField] float rollStabilizationSpeed = 5f;
 
-    [HorizontalLine("Debug Stats", 3, FixedColor.CherryRed)]
-    [SerializeField] float horizontalXInput;
+    [VInspector.Foldout("Steering Debug")]
+    [HorizontalLine("Steering debug Stats", 3, FixedColor.CherryRed)]
+    [ReadOnly][SerializeField] Quaternion targetRotation;
+    [ReadOnly][SerializeField] Quaternion finalRotation;
+    [ReadOnly][SerializeField] float horizontalInputValue;
+    [ReadOnly][SerializeField] float dot;
+    [VInspector.EndFoldout]
 
-    [SerializeField] Quaternion targetRotation;
-    [SerializeField] Quaternion slerpedRotation;
-    [SerializeField] float dot;
+    [HorizontalLine("Speeding/ Vertical Control Settings", 3, FixedColor.DustyBlue)]
+    [SerializeField] KeyCode divingInput;
+    [SerializeField] KeyCode ascendInput;
+    [SerializeField] float buildUpSpeed;
+    [SerializeField] float maxAccelerationBuildUp;
+    [SerializeField] float divingDivider;
+    [SerializeField] float deacceleration;
+    [SerializeField] float pitchRotationSpeed;
+
+    [HorizontalLine("Clamp Settings", 3, FixedColor.DustyBlue)]
+    [SerializeField] float minPitch;
+    [SerializeField] float maxPitch;
+
+    [HorizontalLine("Pitch Control debug", 3, FixedColor.DustyBlue)]
+    [ReadOnly][SerializeField] float currentAccelerationBuildUp;
+    [ReadOnly][SerializeField] Quaternion currentPitchRotation;
+    [ReadOnly] float verticalInputValue;
 
     Vector3 initialForward;
 
@@ -26,57 +47,56 @@ public class AirMovement : MonoBehaviour
         initialForward = transform.forward;
     }
 
-    void FixedUpdate()
+    private void Update()
     {
-        horizontalXInput = Input.GetAxisRaw("Horizontal");
-
-        HandleSteeringAndStabilization();
-        HandleForwardMovement();
-
-        // Debug Rays - let's keep these to visualize what's happening
-        Debug.DrawRay(transform.position, transform.forward * forwardSpeed, Color.green);
-        Debug.DrawRay(transform.position, transform.right * forwardSpeed, Color.red);
-        Debug.DrawRay(transform.position, transform.up * forwardSpeed, Color.blue);
+        if (Input.GetKey(divingInput))
+        {
+            verticalInputValue =1;
+            currentAccelerationBuildUp = Mathf.Lerp(currentAccelerationBuildUp, maxAccelerationBuildUp, Time.deltaTime*buildUpSpeed);
+        }
+        else if (Input.GetKey(ascendInput))
+        {
+            verticalInputValue=-1;
+            currentAccelerationBuildUp = Mathf.Lerp(currentAccelerationBuildUp, 0, Time.deltaTime*deacceleration);
+        }
+        else
+        {
+            currentAccelerationBuildUp = Mathf.Lerp(currentAccelerationBuildUp, 0, Time.deltaTime*deacceleration);
+            verticalInputValue =0;
+        }
     }
 
-    void HandleSteeringAndStabilization()
+    void FixedUpdate()
     {
+        horizontalInputValue = Input.GetAxisRaw("Horizontal");
         Quaternion currentRotation = rb.rotation;
-        Quaternion rollRotation = Quaternion.identity;
-        Quaternion yawRotation = Quaternion.identity;
 
-        if (Mathf.Abs(horizontalXInput) >= 0.1f)
+        #region Horizontal Steering
+        if (Mathf.Abs(horizontalInputValue) >= 0.1f)
         {
-            // Steering based on input
-           
-
-            dot =Vector3.Dot(transform.forward, initialForward) ;// Check if we are oriented "backward-ish" to invert roll
-            Vector3 currentRolldirection = transform.forward;
-            float lerpSpeed = 2f; // Adjustable speed for the Lerp - you can tweak this
+            dot =Vector3.Dot(transform.forward, initialForward);// Check if we are oriented "backward-ish" to invert roll
+            Vector3 currentRollDirection = transform.forward;
+            float lerpSpeed = 2f;
 
             if (dot < 0)
             {
-                currentRolldirection = Vector3.Lerp(transform.forward, -transform.forward, Time.fixedDeltaTime * lerpSpeed);
+                currentRollDirection = Vector3.Lerp(transform.forward, -transform.forward, Time.fixedDeltaTime * lerpSpeed);
             }
             else if (dot > 0)
             {
-                // Let's simplify this for now and focus on the negative dot case
-                // If you need special behavior for positive dot, we can add it back later, tell me what you want to achieve.
-                currentRolldirection = transform.forward; // For positive dot, just use forward direction for now.
-                // If you *do* want to Lerp for positive dot, let me know what you want to Lerp *to* and *why*.
-
-                // Original code for positive dot (if you need to revisit it, but let's simplify for now):
-                // Vector3 absoluteForward = new Vector3(Mathf.Abs(transform.forward.x), Mathf.Abs(transform.forward.y), Mathf.Abs(transform.forward.z));
-                // currentRolldirection = Vector3.Lerp(transform.forward,absoluteForward, Time.fixedDeltaTime * lerpSpeed);
+                currentRollDirection = transform.forward;
             }
 
-            float rollAngle = -horizontalXInput * rollSteeringSpeed * Time.fixedDeltaTime; // Negate for natural control
-            rollRotation = Quaternion.AngleAxis(rollAngle,currentRolldirection); // Roll around the forward axis
+            float rollAngle = -horizontalInputValue * rollSteeringSpeed * Time.fixedDeltaTime; // Negate for natural control
+            Quaternion rollRotation = Quaternion.AngleAxis(rollAngle, currentRollDirection); // Roll around the forward axis
 
-            float yawAngle = horizontalXInput * yawSteeringSpeed * Time.fixedDeltaTime;
-            yawRotation = Quaternion.AngleAxis(yawAngle,Vector3.up); // Yaw around world up axis, not local up
-            slerpedRotation = currentRotation *rollRotation* yawRotation;
+            float yawAngle = horizontalInputValue * yawSteeringSpeed * Time.fixedDeltaTime;
+            Quaternion yawRotation = Quaternion.AngleAxis(yawAngle, Vector3.up); // Yaw around world up axis, not local up
+
+            finalRotation = currentRotation *rollRotation* yawRotation;
         }
+        #endregion
+        #region Horizontal Stabilization Steering
         else
         {
             // Roll Stabilization when no horizontal input
@@ -84,16 +104,36 @@ public class AirMovement : MonoBehaviour
             Vector3 targetEuler = new Vector3(currentEuler.x, currentEuler.y, 0f); // Keep X and Y, zero out Z (roll)
             targetRotation = Quaternion.Euler(targetEuler);
             float slerpT = rollStabilizationSpeed * Time.fixedDeltaTime;
-            slerpedRotation = Quaternion.Slerp(currentRotation, targetRotation, slerpT);
+            finalRotation = Quaternion.Slerp(currentRotation, targetRotation, slerpT);
         }
-        rb.MoveRotation(slerpedRotation); // Apply the rotation
-    }
+        #endregion
+        #region Pitch Rotation Control
 
+        float pitchAngle = verticalInputValue* pitchRotationSpeed * Time.fixedDeltaTime;
+        currentPitchRotation = Quaternion.AngleAxis(pitchAngle, transform.right);
+        finalRotation*= currentPitchRotation;
 
-    void HandleForwardMovement()
-    {
-        // Move forward always in the plane's forward direction
-        Vector3 moveDirection = (transform.forward);
-        rb.MovePosition(rb.position + moveDirection * forwardSpeed); // Use MovePosition for kinematic movement
+        float normalizedAngle = finalRotation.eulerAngles.x;
+        if (normalizedAngle >= 360f || normalizedAngle <= -360f) // More robust wrap-around check
+        {
+            normalizedAngle = Mathf.Repeat(normalizedAngle, 360f); // Use Mathf.Repeat for wrap-around
+            if (normalizedAngle > 180f) normalizedAngle -= 360f; // Normalize to -180 to 180
+        }
+        normalizedAngle = Mathf.Clamp(normalizedAngle,minPitch,maxPitch);
+        finalRotation.eulerAngles = new Vector3(normalizedAngle,finalRotation.eulerAngles.y,finalRotation.eulerAngles.z);
+
+        #endregion
+
+        rb.MoveRotation(finalRotation); // Apply the rotation
+
+        rb.MovePosition(rb.position + transform.forward * forwardSpeed);
+        if (verticalInputValue ==1|| verticalInputValue == 0)
+        {
+            rb.AddForce(transform.forward * currentAccelerationBuildUp/divingDivider, ForceMode.Acceleration);
+        }
+        else if (verticalInputValue ==-1)
+        {
+            rb.AddForce(transform.forward * currentAccelerationBuildUp, ForceMode.Acceleration);
+        }
     }
 }
